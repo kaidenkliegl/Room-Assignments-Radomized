8a8bb8ed49d376ba375cb6a9ded476ca02ad2e03
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2024-10-02 15:23:19 -0700

    finished code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Room-Assignments-Radomized
-randomly assign rooms
+Randomly assign rooms:
+
+## Setup
+1. Run `npm install prompt-sync` to install all dependencies.
+3. Run `node 'file-name` to run file.
+4. enter in the information prompted in the terminal to excute the code.
+
---
+++ randomizer.js
@@ -0,0 +1,49 @@
+const prompt = require("prompt-sync")();
+
+let array = [];
+//get the number of rooms and turn to a number
+let nameAmount = prompt("Enter the amount of names: ");
+nameAmount = Number(nameAmount);
+
+//get all the names of the students
+let PickName = () => {
+  let userInput = prompt("Enter your name: ");
+  amount = nameAmount;
+  //push to the array
+  array.push(userInput);
+  if (array.length !== nameAmount) {
+    PickName();
+  }
+};
+//randomize the names in the array
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+//create rooms
+let roomAssignment = (num, array) => {
+//   console.log(array);
+  let obj = {};
+
+  for (let index = 1; index <= num; index++) {
+    let person1 = array.shift();
+    let person2 = array.shift();
+    obj[`room ${index}`] = `${person1}, ${person2}`;
+  }
+  if (array.length === 1) {
+    let value = obj[`room ${num}`];
+    obj[`room ${num}`] = value.concat(`, ${array[0]}`);
+  }
+  for(let key in obj){
+    console.log('----THESE ARE THE ROOM ASSIGNMENTS----' )
+    console.log(key + ': ' + obj[key])
+  }
+};
+
+PickName();
+let arrayNames = shuffle(array);
+let numberOfRooms = Math.floor(nameAmount / 2);
+roomAssignment(numberOfRooms, arrayNames);
